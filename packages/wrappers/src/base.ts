67fab12909e84da71ea1b622d41e8c440aab4b87
Viren070 <viren070@protonmail.com>
2025-04-28 20:13:51 +0100

    fix: allow newlines when parsing for service data

--- packages/wrappers/src/base.ts
+++ packages/wrappers/src/base.ts
@@ -422,7 +422,7 @@ export class BaseWrapper {
     services.forEach((service) => {
       // for each service, generate a regexp which creates a regex with all known names separated by |
       const regex = new RegExp(
-        `(^|(?<![^ |[(_\\/\\-.]))(${service.knownNames.join('|')})(?=[ ⬇️⏳⚡+/|\\)\\]_.-]|$)`,
+        `(^|(?<![^ |[(_\\/\\-.]))(${service.knownNames.join('|')})(?=[ ⬇️⏳⚡+/|\\)\\]_.-]|$|\n)`,
         'i'
       );
       // check if the string contains the regex
