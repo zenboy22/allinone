72b5ab648e511220d7ff8b4bf453db94bb952b30
Munif Tanjim <hello@muniftanjim.dev>
2025-05-12 18:56:39 +0600

    feat: stremthru improvements (#172)

--- packages/utils/src/stremthru.ts
+++ packages/utils/src/stremthru.ts
@@ -46,6 +46,9 @@ export async function generateStremThruStreams(
       }
     }
     data.append(`req_headers[${i}]`, req_headers);
+    if (stream.filename) {
+      data.append(`filename[${i}]`, stream.filename);
+    }
   });
 
   if (Settings.ENCRYPT_STREMTHRU_URLS) {
@@ -155,7 +158,10 @@ export async function getStremThruPublicIp(
     }
 
     const data = await response.json();
-    const publicIp = data.data?.ip?.machine;
+    const publicIp =
+      typeof data.data?.ip?.exposed === 'object' // available from `v0.71.0`
+        ? data.data.ip.exposed['*'] || data.data.ip.machine
+        : data.data?.ip?.machine;
     if (publicIp && cache) {
       cache.set(cacheKey, publicIp, Settings.CACHE_STREMTHRU_IP_TTL);
     } else {
