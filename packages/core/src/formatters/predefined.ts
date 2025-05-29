6fa1b2b0c0cb45e9344163989009238d528d330b
Viren070 <viren070@protonmail.com>
2025-05-29 18:34:53 +0100

    feat: add built-in torrentio format

--- packages/core/src/formatters/predefined.ts
+++ packages/core/src/formatters/predefined.ts
@@ -3,8 +3,17 @@ import { BaseFormatter, FormatterConfig } from './base';
 export class TorrentioFormatter extends BaseFormatter {
   constructor() {
     super({
-      name: '{stream.title} {stream.quality}',
-      description: '{stream.size::bytes} {stream.seeders} seeders',
+      name: `
+{stream.proxied::istrue["🕵️‍♂️ "||""]}{stream.type::=p2p["[P2P] "||""]}{service.id::exists["[{service.shortName}"||""]}{service.cached::istrue["+] "||""]}{service.cached::isfalse[" download] "||""]}{addon.name} {stream.resolution::exists["{stream.resolution}"||"Unknown"]}
+{stream.visualTags::exists["{stream.visualTags::join(' | ')}"||""]}      
+`,
+      description: `
+{stream.message::exists["ℹ️{stream.message}"||""]}
+{stream.folderName::exists["{stream.folderName}"||""]}
+{stream.filename::exists["{stream.filename}"||""]}
+{stream.size::>0["💾{stream.size::bytes2} "||""]}{stream.seeders::>0["👤{stream.seeders} "||""]}{stream.age::exists["📅{stream.age} "||""]}{stream.indexer::exists["⚙️{stream.indexer}"||""]}
+{stream.languageEmojis::exists["{stream.languageEmojis::join( / ')}"||""]}
+`,
     });
   }
 }
